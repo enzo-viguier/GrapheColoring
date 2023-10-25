028301d1e7cabbe1ba8ee86ef3be2a1d1eeae92b
enzo-viguier <enzoviguier@outlook.fr>
2023-10-25 22:43:42 +0200

    Initial Commit

---
+++ src/test/java/TestTemplate.java
@@ -0,0 +1,21 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestTemplate {
+
+
+
+    @Before
+    public void setUp() {
+        //TODO : Initialiser les variables
+    }
+
+    @Test
+    public void testMethod() {
+        int result = 8;
+        assertEquals(8, result); // Vérifie que 5 + 3 = 8
+    }
+
+}
